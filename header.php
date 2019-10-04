628ffce485ab317a282300e536ed28c98aa42b02
perrine <perrine98@live.fr>
2019-10-04 17:28:55 +0200

    header

---
+++ header.php
@@ -0,0 +1,25 @@
+<head>
+        <meta charset="iso-8859-1">
+        <title>PINEAPPLE</title>
+        <link rel="stylesheet" href="style.css">
+</head>
+<header>
+    <h1> PINEAPPLE</h1> 
+    <marginleft>
+        <p>
+        <a href='productSearch.php'> Recherche de produits </a></p>
+        <a href='product.php'> Produits  </a>
+    </leftside>
+    <center>
+            <img src="logo.jpg">
+    </center>
+    <right>
+        <aside>
+            <a href='logUp.php'> Inscription  </a>
+            <a href='logIn.php'> Connexion  </a>  
+        </aside>
+        <a href='Cart.php'>Panier </a>
+    </right>
+</header>
+        
+
