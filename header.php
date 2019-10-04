f14137566a60df041d60d8de6a1ade2148467d75
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-10-04 21:12:15 +0200

    CSS

--- header.php
+++ header.php
@@ -3,15 +3,25 @@
         <title>PINEAPPLE</title>
         <link rel="stylesheet" href="style.css">
 
-    <a href = 'index.php' class = "titre">PINEAPPLE</a>
+
+    <div id = "logoNom">
 
     <img src="logo.jpg" class = "logo">
+
+    <a href = 'index.php' class = "titre">PINEAPPLE</a>
+
+    <div class = "vide"></div>
+
+
+    </div>
     
-        
+    <p>   
     <a href='productSearch.php' class ="menu"> Produits </a>      
     <a href='logUp.php' class ="menu"> Inscription  </a>
     <a href='logIn.php' class ="menu"> Connexion  </a>  
-    <a href='Cart.php' class ="menu">Panier </a>
+    <a href='Cart.php' class ="menu">Panier </a></p>
+
+    
   
 
         
