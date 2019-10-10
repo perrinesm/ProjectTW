9893811b7aff60323fedb0c3446ddaa168aff471
perrine <perrine98@live.fr>
2019-10-10 17:23:07 +0200

    Tableau pour ordi

--- tabOrdi.php
+++ tabOrdi.php
@@ -15,32 +15,84 @@
         <h2 style="margin-left: 90px;">  Ordinateurs </h2>
 
             <div class = "product">  
-                <h4>  <a href="ordiA1.php"> MacPin Pro </a></h4>
-                <p> <img src="ordiA1.png" /> </p>
-                <p class="description"><em><strong> Description </strong> </em> <center><em><strong> Prix  </strong> </em></center></p>
-                <p class="description"> Une incroyable puissance. Au meilleur prix. Waterproof .<br >Bulletproof <br /><br /> Puce A12 Bionic avec Neural Engine. Écran Retina de 10,5 pouces avec affichage True Tone </p>
+                <style>
+                    table{
+                        border-collapse:collapse;}
+                    
+                </style>
+                <table width="75%">
+                    <thead><tr>
+                        <th align="left"><h4>  <a href="ordiA1.php"> MacPin Pro </a></h4></th>
+                        <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                        <th align="center"><em><strong> Prix  </strong> </em></th>
+                    </tr></thead>
+                    <tbody><tr>
+                        <td ><p> <img src="ordiA1.png" /> </p></td>
+                        <td><p class="description"> Une incroyable puissance. Au meilleur prix. Waterproof .<br >Bulletproof <br /><br /> Puce A12 Bionic avec Neural Engine. Écran Retina de 10,5 pouces avec affichage True Tone </p></td>
+                        <td align="center"><p> 25,99€ </p></td>
+                    </tr></tbody>
+                </table>
                 
             </div>
     
             <div class = "product">
-                <h4> <a href="ordiA2.php"> PinTosh 1984 </a> </h4>
-                <p> <img src="ordiA2.png" /> </p>
-                <p class="description"> <em><strong>Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-                <p> <center>25,99€ </center></p>
+                <style>
+                    table{
+                        border-collapse:collapse;}
+                    
+                </style>
+                <table width="75%">
+                    <thead><tr>
+                        <th align="left"><h4> <a href="ordiA2.php"> PinTosh 1984 </a> </h4></th>
+                        <th align="left"><p class="description"> <em><strong>Description </strong> </em></p></th>
+                        <th align="center"><em><strong> Prix  </strong> </em></th>
+                    </tr></thead>
+                    <tbody><tr>
+                        <td><p> <img src="ordiA2.png" /> </p></td>
+                        <td><p class="description"> à compléter ;;;;;;;;;;;;;;;;;;;;;;;;;;;;LLLLLLLLLLLLLLLLLLL LLLL?KKKKKKKKKKKKKKKK <br >idem <br /><br /> idem</p></td>
+                        <td align="center"><p> 25,99€ </p></td>
+                    </tr></tbody>
+                </table>
             </div>
     
             <div class = "product">
-                <h4 > <a href="ordiS1.php">Universe Pin-BOOK</a> </h4>
-                <p> <img  src="ordiS1.png" /> </p>
-                <p class="description"><em><strong> Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-                <p> <center>25,99€ </center></p>
+                <style>
+                    table{
+                        border-collapse:collapse;}
+                    
+                </style>
+                <table width="75%">
+                    <thead><tr>
+                        <th align="left"><h4 > <a href="ordiS1.php">Universe Pin-BOOK</a> </h4></th>
+                        <th align="left"> <p class="description"><em><strong> Description </strong> </em></p></th>
+                        <th align="center"><em><strong> Prix  </strong> </em></th>
+                    </tr></thead>
+                    <tbody><tr>
+                        <td><p> <img  src="ordiS1.png" /> </p></td>
+                        <td><p class="description"> à compléter <br >idem <br /><br /> idem</p></td>
+                        <td align="center"><p> 25,99€ </p></td>
+                    </tr></tbody>
+                </table>    
             </div>
     
             <div class = "product">
-                <h4> <a href="ordiS1.php"> SurvivorPin 2.0 </a>  </h4>
-                <p> <img src="ordiS2.png" /> </p>
-                <p class="description"> <em><strong>Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-                <p class="description"> Produit sponsorisé par Bear Grill. <center>25,99€ </center> <br  /> Utilisable dans toutes conditions météorologiques. Recommandé en zone de conflit armé. </p>
+                <style>
+                    table{
+                        border-collapse:collapse;}
+                    
+                </style>
+                <table width="75%">
+                    <thead><tr>
+                        <th align="left"><h4> <a href="ordiS1.php"> SurvivorPin 2.0 </a>  </h4></th>
+                        <th align="left"><p class="description"> <em><strong>Description </strong> </em></p></th>
+                        <th align="center"><em><strong> Prix  </strong> </em></th>
+                    </tr></thead>
+                    <tbody><tr>
+                        <td><p> <img src="ordiS2.png" /> </p></td>
+                        <td><p class="description"> Produit sponsorisé par Bear Grill.<br  /> Utilisable dans toutes conditions météorologiques. Recommandé en zone de conflit armé. </p></td>
+                        <td align="center"><p> 25,99€ </p></td>
+                    </tr></tbody>
+                </table>
             </div>
 
     </body>
