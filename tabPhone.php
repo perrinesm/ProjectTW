4e6ec675e3e28f90af8e1a277523a6f5d3968ec8
perrine <perrine98@live.fr>
2019-10-10 22:32:16 +0200

    modif tableau

--- tabPhone.php
+++ tabPhone.php
@@ -17,32 +17,83 @@
         <h2 style="margin-left: 90px;">  Téléphones </h2>
 
         <div class = 'product'>  
-            <h4 > <a href="telA1.php">  Pinphone XS </a> </h4>
-            <p> <img src="telA1.png" /> </p>
-            <p class="description"> <em><strong>Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-            <p class="description"> Une incroyable puissance. Au meilleur prix. Waterproof . <center>25,99€ </center><br />Bulletproof <br /><br /> </p>
-            
+            <style>
+                table{
+                   border-collapse:collapse;}
+                    
+            </style>
+            <table width="75%">
+                <thead><tr>
+                    <th align="left"> <h4 > <a href="telA1.php">  Pinphone XS </a> </h4></th>
+                    <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                    <th align="center"><em><strong> Prix  </strong> </em></th>
+                </tr></thead>
+                <tbody><tr>
+                    <td ><p> <img src="telA1.png" /> </p></td>
+                    <td><p class="description"> Une incroyable puissance. <br >Au meilleur prix. <br >Waterproof .<br />Bulletproof</p></td>
+                    <td align="center"><p> 899,99€ </p></td>
+                </tr></tbody>
+            </table>
         </div>
 
         <div class = 'product'>
-            <h4> <a href="telA2.php">  Pinphone Xmax </a> </h4>
-            <p> <img src="telA2.png" /> </p>
-            <p class="description"> <em><strong>Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-            <p><center>25,99€ </center></p>
+            <style>
+                table{
+                   border-collapse:collapse;}
+                    
+            </style>
+            <table width="75%">
+                <thead><tr>
+                    <th align="left"> <h4> <a href="telA2.php">  Pinphone Xmax </a> </h4></th>
+                    <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                    <th align="center"><em><strong> Prix  </strong> </em></th>
+                </tr></thead>
+                <tbody><tr>
+                    <td ><p> <img src="telA2.png" /> </p></td>
+                    <td><p class="description"> Une incroyable puissance. <br >Au meilleur prix. <br >Waterproof .<br />Bulletproof</p></td>
+                    <td align="center"><p> 1159,99€ </p></td>
+                </tr></tbody>
+            </table>
         </div>
 
         <div class = 'product'>
-            <h4>  <a href="telS1.php">  Pineapple 3310x </a> </h4>
-            <p> <img  src="telS1.png" /> </p>
-            <p class="description"> <em><strong>Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-            <p><center>25,99€ </center></p>
+            <style>
+                table{
+                   border-collapse:collapse;}
+                    
+            </style>
+            <table width="75%">
+                <thead><tr>
+                    <th align="left"> <h4>  <a href="telS1.php">  Pineapple 3310x </a> </h4></th>
+                    <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                    <th align="center"><em><strong> Prix  </strong> </em></th>
+                </tr></thead>
+                <tbody><tr>
+                    <td ><p> <img src="telS1.png" /> </p></td>
+                    <td><p class="description"> Une incroyable puissance. <br >Au meilleur prix. <br >Waterproof .<br />Bulletproof</p></td>
+                    <td align="center"><p> 659,99€ </p></td>
+                </tr></tbody>
+            </table>
         </div>
 
         <div class = 'product'>
-            <h4> <a href="telS2.php">PinGalaxy S9 </a> </h4>
-            <p> <img src="telS2.png" /> </p>
-            <p class="description"><em><strong> Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-            <p><center>25,99€ </center></p>
+            <style>
+                table{
+                   border-collapse:collapse;}
+                    
+            </style>
+            <table width="75%">
+                <thead><tr>
+                    <th align="left"> <h4> <a href="telS2.php">PinGalaxy S9 </a> </h4></th>
+                    <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                    <th align="center"><em><strong> Prix  </strong> </em></th>
+                </tr></thead>
+                <tbody><tr>
+                    <td ><p> <img src="telS2.png" /> </p></td>
+                    <td><p class="description"> Une incroyable puissance. <br >Au meilleur prix. <br >Waterproof .<br />Bulletproof</p></td>
+                    <td align="center"><p> 659,99€ </p></td>
+                </tr></tbody>
+            </table>
         </div>
 
     </body>
