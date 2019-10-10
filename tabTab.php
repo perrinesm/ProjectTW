b3aa4294187a5484451431696ccc66fb74fd346d
perrine <perrine98@live.fr>
2019-10-10 22:35:18 +0200

    tab en partie

--- tabTab.php
+++ tabTab.php
@@ -14,12 +14,25 @@
     <body  style="display: flex; flex-direction: column;">
         <h2 style="margin-left: 90px;">  Tablettes </h2>
 
-        <div class = 'product'>  
-            <h4 style="margin-top: 5px;"> <a href="tabA1.php"> PinPad Pro </a></h4>
-            <p> <img src="tabA1.png" /> </p>
-            <p class="description"><em><strong> Description </strong> </em><center><em><strong> Prix  </strong> </em></center></p>
-            <p class="description"> Meilleure tablette du marché Il est tout nouveau, tout écran, tout puissant. <center>25,99€ </center><br /> Il renferme nos technologies les plus avancées dans un design complètement réinventé..<br />Waterproof<br /><br /> </p>
-        </div>
+        <div class = 'product'> 
+            <style>
+                table{
+                   border-collapse:collapse;}
+                    
+            </style>
+            <table width="75%">
+                <thead><tr>
+                    <th align="left"> <h4 style="margin-top: 5px;"> <a href="tabA1.php"> PinPad Pro </a></h4></th>
+                    <th align="left"><p class="description"><em><strong> Description </strong></em></p></th>
+                    <th align="center"><em><strong> Prix  </strong> </em></th>
+                </tr></thead>
+                <tbody><tr>
+                    <td ><p> <img src="tabA1.png" /> </p></td>
+                    <td><p class="description"> Meilleure tablette du marché Il est tout nouveau, tout écran, tout puissant. <br >Il renferme nos technologies les plus avancées dans un design complètement réinventé.. <br >Waterproof .</p></td>
+                    <td align="center"><p> 659,99€ </p></td>
+                </tr></tbody>
+            </table> 
+      </div>
 
         <div class = 'product'>
             <h4> <a href="tabA2.php"> PinSmart </a> </h4>
