318fd0da665c42f26497a30497f3d2bdecf12fd3
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-10-11 20:21:16 +0200

    New Footer

--- footer.php
+++ footer.php
@@ -1,14 +1,39 @@
 
-<link rel="stylesheet" href="stylesheet.css">
 
-<a href="mailto:antoine.genonceau@isen.yncrea.fr"> <p class = "contact">Contactez nous</p></a>
+   <div class="footer">
+     <div class="footer-content">
+       <div class="footer-section">
+         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> <span>  About Us</span> </h1>       
+         <p style="padding-left: 10px;"> Pineapple est une société de conception d'appareils multimédia. <br/>
+            Nous vendons nos produits à l'international. <br/>
+            Et c'est déjà pas mal ! </p>
+          <img src="logofb.png" style="padding-left: 8px; width: 15%; height: 15%;" />
+          <img src="logoInsta.png" style="padding-left: 8px; width: 15%; height: 15%;" />
+          <img src="logotwi.png" style="padding-left: 8px; width: 15%; height: 15%;" />
+          <p style="color: darkblue"> MAIL :  pineapple9a7ba@pineapple-contact.com </p>
+       </div>
 
+       <div class="footer-section">   
+         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> Service SAV </h1>
+         <p>  
+            Nos conseillers spécialisés vous assurent un décroché rapide ainsi qu’une réponse personnalisée 24H/24 et 7J/7. <br/>
+            Le 33622302153 vous mettra en relation avec le service désiré afin que vous puissiez trouver la réponse rapide <br/>
+             à vos interrogations ou simplement obtenir le renseignement voulu.
+         </p>
 
+      </div>
 
+       <div class="footer-section">
+         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> Contact Us </h1>  <br/>
+         <form action="index.php" method="post" style = "display: flex; flex-direction : column; padding: 10px"> <br/>
+           <input type="email" name="email"  style="width: 60%; background-color: grey" placeholder="Votre adresse email..." > 
+           <textarea name="message" style="height: 90px; background-color: grey;"; placeholder="Votre message..."> </textarea> 
+           <button type="submit" style="width: 30%; background-color: darkgreen;">
+             Envoyer
+           </button>
+         </form>
+      </div>
 
-
-
-
-
-
-
+     </div>
+  
+  </div>
