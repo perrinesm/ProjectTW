90263a028cc32be52d9f2f60ef1d020c21e2ce01
perrine <perrine98@live.fr>
2019-10-11 22:02:56 +0200

    police

--- footer.php
+++ footer.php
@@ -1,39 +1,35 @@
 
-
-   <div class="footer">
-     <div class="footer-content">
-       <div class="footer-section">
-         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> <span>  About Us</span> </h1>       
-         <p style="padding-left: 10px;"> Pineapple est une société de conception d'appareils multimédia. <br/>
-            Nous vendons nos produits à l'international. <br/>
-            Et c'est déjà pas mal ! </p>
-          <img src="logofb.png" style="padding-left: 8px; width: 15%; height: 15%;" />
-          <img src="logoInsta.png" style="padding-left: 8px; width: 15%; height: 15%;" />
-          <img src="logotwi.png" style="padding-left: 8px; width: 15%; height: 15%;" />
-          <p style="color: darkblue"> MAIL :  pineapple9a7ba@pineapple-contact.com </p>
-       </div>
-
-       <div class="footer-section">   
-         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> Service SAV </h1>
-         <p>  
-            Nos conseillers spécialisés vous assurent un décroché rapide ainsi qu’une réponse personnalisée 24H/24 et 7J/7. <br/>
-            Le 33622302153 vous mettra en relation avec le service désiré afin que vous puissiez trouver la réponse rapide <br/>
-             à vos interrogations ou simplement obtenir le renseignement voulu.
-         </p>
-
-      </div>
-
-       <div class="footer-section">
-         <h1 style="font-family : 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'sans-serif'"> Contact Us </h1>  <br/>
-         <form action="index.php" method="post" style = "display: flex; flex-direction : column; padding: 10px"> <br/>
-           <input type="email" name="email"  style="width: 60%; background-color: grey" placeholder="Votre adresse email..." > 
-           <textarea name="message" style="height: 90px; background-color: grey;"; placeholder="Votre message..."> </textarea> 
-           <button type="submit" style="width: 30%; background-color: darkgreen;">
-             Envoyer
-           </button>
-         </form>
-      </div>
-
-     </div>
-  
+<div class="footer">
+  <div class="footer-content">
+    <div class="footer-section">
+      <style>
+        table{
+          border-collapse:collapse;}  
+      </style>
+      <table width="100%">
+        <thead><tr>
+          <th align="center" width="33%"><h1 style="font-family : fantasy"> <span>  About Us</span> </h1></th>
+          <th align="center" width="33%"><h1 style="font-family : fantasy"> Service SAV </h1></th>
+          <th align="center" width="33%"><h1 style="font-family : fantasy"> Contact Us </h1>  </th>
+        </tr></thead>
+        <tbody><tr>
+          <td align="center" width="33%"><p style="padding-left: 10px;"> Pineapple est une société de conception d'appareils multimédia. <br/>Nous vendons nos produits à l'international. <br/>Et c'est déjà pas mal ! </p> </td>
+          <td align="center" width="33%"><p>  Nos conseillers spécialisés vous assurent un décroché rapide ainsi qu’une réponse personnalisée 24H/24 et 7J/7. <br/>Le 33622302153 vous mettra en relation avec le service désiré afin que vous puissiez trouver la réponse rapide <br/>à vos interrogations ou simplement obtenir le renseignement voulu.</p></td>
+          <td align="center" width="33%"><p><form action="index.php" method="post" style = "display: flex; flex-direction : column; padding: 10px; align=center;"> <br/>
+            <input type="email" name="email"  style="align=center; width: 60%; background-color: grey" placeholder="Votre adresse email..." > 
+            <textarea name="message" style="align=center; height: 90px; background-color: grey;"; placeholder="Votre message..."> </textarea> 
+            <button type="submit" style="width: 30%; background-color: darkgreen;align=center;">
+              Envoyer
+            </button>
+          </form></p></td>
+        </tr><tr >
+          <td width="15%" align="center"><p><img src="logofb.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" />
+            <img src="logoInsta.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" />
+            <img src="logotwi.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" /></p> </td>
+        </tr><tr align="center">
+          <td width="15%"><p style="color: darkblue"> MAIL :  pineapple9a7ba@pineapple-contact.com </p></td>
+        </tr></tbody>
+      </table> 
+    </div>
   </div>
+</div>
