b9d87555b0e4bb7cf6e7a5119ac420a01523e642
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-10-11 16:40:14 +0200

    New Header

--- header.php
+++ header.php
@@ -1,26 +1,74 @@
 
         <meta charset="iso-8859-1">
         <title>PINEAPPLE</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="stylesheet.css">
+
+    <div ID="headerID">
 
 
-
-
-    <div id="logoNom">
-    <a href = 'index.php' class = "titre">PINEAPPLE</a>
-    <img src="logo.png" class = "logo"> 
-    </div>
+        <div id="logoNom">
+            <a href = 'index.php' class = "titre">PINEAPPLE</a>
+            <img src="logo.png" class = "logo"> 
+            
+        </div>
 
 
        
-    <p>   
-    <a href='productSearch.php' class ="menu"> Produits </a>      
-    <a href='logUp.php' class ="menu"> Inscription  </a>
-    <a href='logIn.php' class ="menu"> Connexion  </a>  
-    <a href='Cart.php' class ="menuCart"><img class="logoCart" src="panier.png"></a></p>
+        <div id="bande">   
+
+            <div id="bandeGauche">
+               
+                <div class="menuCategory">
+
+                    <span class="navtitle">Produit</span>
+                <div class="withSeveralLinks">
+                    
+                <a href = 'tabPhone.php' class = "menu">Téléphones</a>
+                <a href = 'tabTab.php' class = "menu">Tablettes</a>
+                <a href = 'tabOrdi.php' class = "menu">Ordinateurs</a>
+                <a href = 'tabAcc.php' class = "menu">Accessoires</a>
+            
+
+
+            </div>
+            </div>
+
+
+                <meta class="menu">Recherche :</meta>
+
+                <meta><input type="text" name="ID" id="ID" /></meta>
+
+                <meta class="menu"><input type="submit" class="Bouton" value="OK"/></meta>
+
+
+            </div>
+
+            <div id="bandeDroite">
+
+                <form method="post" action="index.php"> 
+
+                <meta class="menu">ID :</meta>
+
+                <meta class="menu"><input type="text" name="ID" id="ID" /></meta>
+
+                <meta class="menu">Password :</meta>
+
+                <meta class="menu"><input type="text" name="PW" id="PW" ></meta>
+
+                <meta class="menu"><input type="submit" class="Bouton" value="OK"/></meta>
+
+                <a href='logUp.php' class="menu">Inscription</a>
+
+                <a href = 'cart.php'><img class="logoCart" src="panier.png"></a>
+                
+                </form>
+   
+            </div>
+
+        </div>
 
     
-  
+    </div>
 
         
 
