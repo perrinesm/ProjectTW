1d8978a27275ac564e4a5ee2a30d121f86c6a9e6
perrine <perrine98@live.fr>
2019-10-11 13:45:44 +0200

    logup

--- logUp.php
+++ logUp.php
@@ -2,7 +2,7 @@
 
 <head>
 
-  <link rel="stylesheet" href="style.css">  
+  <link rel="stylesheet" href="stylesheet.css">  
   <link rel="icon" type="image/png" href="icon.png" />	
 
 </head>
@@ -17,38 +17,20 @@
 </header>
 
 <body>
-	
-	<form method="post" action="logIn.php"> 	
-   			
-   		
-   		<p>ID :
-
-   		<input type="text" name="ID" id="ID"/>
-
-     	</p>
-
-
-
-
-   		<p>Mot de passe :
-
-   		<input type="text" name="PW" id="PW"/>
-   		
-   	    </p>
-
-
-   	    <p>E-mail :
-
-   		<input type="text" name="mail" id="mail"/>
-   		
-   	    </p>
-
-
-        <input type="submit" class="Bouton" value="OK"/>
-
-	</form>
-
-
+	<div align = "center" class='inscription'>
+		<form method="post" action="logIn.php"> 	
+   			<p>ID :
+   				<input type="text" name="ID" id="ID"/>
+     		</p>
+   			<p>Mot de passe :
+   				<input type="text" name="PW" id="PW"/>
+   	    	</p>
+   	    	<p>E-mail :
+			   <input type="text" name="mail" id="mail"/>
+			</p>
+	        <input type="submit" class="Bouton" value="OK"/>
+		</form>
+	</div>
 </body>
 
 
