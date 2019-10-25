481e5e7a8efc88035ae47678d2a34cb0c57c39ff
perrine <perrine98@live.fr>
2019-10-25 16:59:54 +0200

    footer size

--- footer.php
+++ footer.php
@@ -4,7 +4,8 @@
     <div class="footer-section">
       <style>
         table{
-          border-collapse:collapse;}  
+          border-collapse:collapse;
+        }  
       </style>
       <table width="100%">
         <thead><tr>
@@ -13,8 +14,8 @@
           <th align="center" width="33%"><h1 style="font-family : fantasy"> Contact Us </h1>  </th>
         </tr></thead>
         <tbody ><tr>
-          <td align="center" width="33%"><p style="padding-left: 10px;font-size: 30px;"> Pineapple est une société de conception d'appareils multimédia. <br/>Nous vendons nos produits à l'international. <br/>Et c'est déjà pas mal ! </p> </td>
-          <td align="center" width="33%"><p style="font-size: 30px;">  Nos conseillers spécialisés vous assurent un décroché rapide ainsi qu’une réponse personnalisée 24H/24 et 7J/7. <br/>Le 33622302153 vous mettra en relation avec le service désiré afin que vous puissiez trouver la réponse rapide <br/>à vos interrogations ou simplement obtenir le renseignement voulu.</p></td>
+          <td align="center" width="33%"><p style="padding-left: 10px;"> Pineapple est une société de conception d'appareils multimédia. <br/>Nous vendons nos produits à l'international. <br/>Et c'est déjà pas mal ! </p> </td>
+          <td align="center" width="33%"><p style="">  Nos conseillers spécialisés vous assurent un décroché rapide ainsi qu’une réponse personnalisée 24H/24 et 7J/7. <br/>Le 33622302153 vous mettra en relation avec le service désiré afin que vous puissiez trouver la réponse rapide <br/>à vos interrogations ou simplement obtenir le renseignement voulu.</p></td>
           <td align="center" width="33%"><p><form action="index.php" method="post" style = "display: flex; flex-direction : column; padding: 10px; align=center;"> <br/>
             <input type="email" name="email"  style="align=center; width: 60%; background-color: grey" placeholder="Votre adresse email..." > 
             <textarea name="message" style="align=center; height: 90px; background-color: grey;"; placeholder="Votre message..."> </textarea> 
@@ -23,9 +24,9 @@
             </button>
           </form></p></td>
         </tr><tr >
-          <td width="15%" align="center"><p><img src="logofb.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" />
-            <img src="logoInsta.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" />
-            <img src="logotwi.png" style="align=center; padding-left: 8px; width: 15%; height: 15%;" /></p> </td>
+          <td width="15%" align="center"><p><img src="logofb.png" style="align=center; padding-left: 8px; width: 15%; height: 50px;" />
+            <img src="logoInsta.png" style="align=center; padding-left: 8px; width: 15%; height: 50px;" />
+            <img src="logotwi.png" style="align=center; padding-left: 8px; width: 15%; height: 50px;" /></p> </td>
         </tr><tr align="center">
           <td width="15%"><p style="color: darkblue"> MAIL :  pineapple9a7ba@pineapple-contact.com </p></td>
         </tr></tbody>
