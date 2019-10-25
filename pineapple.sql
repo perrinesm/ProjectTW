61cf2d683f2b8517d50094202acd991ee81cc409
Pierre-Maxime <pierre-maxime.costa@isen.yncrea.fr>
2019-10-25 16:31:31 +0200

    BDDFINAL3.0

--- pineapple.sql
+++ pineapple.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Ven 25 Octobre 2019 à 16:03
+-- Généré le :  Ven 25 Octobre 2019 à 16:31
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
