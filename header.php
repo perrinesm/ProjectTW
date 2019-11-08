6f4dcbf21fa7022bb49620259f279fca2be6b317
Antoine Genonceau <antoine.genonceau@isen.yncrea.fr>
2019-11-08 13:20:40 +0100

    Ajout connexion

--- header.php
+++ header.php
@@ -54,19 +54,51 @@
 
             </div>
 
+            <?php
+
+            include "bdd.php";
+
+            if(isset($_POST['formconnect']))
+{
+    $mailconnect = htmlspecialchars($_POST['mailconnect']);
+    $mdpconnect = sha1($_POST['mdpconnect']);
+    if(!empty($mailconnect) AND !empty($mdpconnect))
+    {
+        $requser = $bdd -> prepare("SELECT * FROM membres WHERE mail = ? AND motdepasse= ?" );
+        $requser -> execute(array($mailconnect,$mdpconnect));
+        $userexist =$requser ->rowCount();
+        if ($userexist==1){
+            $userinfo=$requser->fetch();
+            $_SESSION['id']=$userinfo['id'];
+            $_SESSION['pseudo']=$userinfo['pseudo'];
+            $_SESSION['mail']=$userinfo['mail'];
+            header("Location: profil.php?id=".$_SESSION['id']);
+
+        }
+        else{
+            $erreur="Mauvais mail ou mot de passe";
+        }
+    }
+    else{
+        $erreur="Compléter tous les champs";
+    }
+}
+?>
+
+
             <div id="bandeDroite">
 
-                <form method="post" action="index.php"> 
+                <form method="POST" action=" "> 
 
-                <meta class="menu">ID :</meta>
+                <meta class="menu">E-Mail :</meta>
 
-                <meta class="menu"><input type="text" name="ID" id="ID" /></meta>
+                <meta class="menu"><input type="text" name="mailconnect" id="ID" /></meta>
 
                 <meta class="menu">Password :</meta>
 
-                <meta class="menu"><input type="text" name="PW" id="PW" ></meta>
+                <meta class="menu"><input type="text" name="mdpconnect" id="PW" ></meta>
 
-                <meta class="menu"><input type="submit" class="Bouton" value="OK"/></meta>
+                <meta class="menu"><input type="submit" class="Bouton" name="formconnect" value="OK"/></meta>
 
                 <a href='logUp.php' class="menu">Inscription</a>
 
